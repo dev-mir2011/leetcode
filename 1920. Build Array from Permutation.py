1599430c8eaa77146a84be7286db80a427128243
dev-mir2011 <ruhaanmir.w@gmail.com>
2025-09-15 20:36:50 +0530

    Solution to LeetCode problem 1920

---
+++ 1920. Build Array from Permutation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        
+        return ans
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
